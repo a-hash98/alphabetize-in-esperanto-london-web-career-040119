2b47438d1b6f2389bb42ca0863355b759b6ce92b
Ay Has <ahashim983@yahoo.com>
2019-04-01 22:45:59 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,6 @@
 def alphabetize(word_list)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   sorter = alphabet.split("")
-  for i in word_list
-    puts i
-    puts sorter.index(i[0])
-  end
-  puts word_list.sort_by{|x| sorter.index(x[0])}
+  return word_list.sort_by{|x| sorter.index(x[0])}
 end
-alphabetize(["ŝab","ĉab","fab"])
+puts alphabetize(["ŝab","ĉab","fab"])
